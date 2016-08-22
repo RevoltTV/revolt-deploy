dc599b5e6b3b3d488db455155773aeed88007a36
Andrew Burgess <andrew@andrewburgess.io>
2016-08-22 12:17:50 -0700

    :bug: fix(docker): add email option for legacy docker versions

--- src/commands/docker.js
+++ src/commands/docker.js
@@ -18,7 +18,7 @@ export function login() {
     .then(({ user, password, endpoint }) => {
         console.log(chalk.dim('logging into docker...'));
 
-        return spawn('docker', ['login', '-u', user, '-p', password, endpoint]);
+        return spawn('docker', ['login', '-u', user, '-p', password, '-e', 'none', endpoint]);
     });
 }
 
